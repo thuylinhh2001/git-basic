80eb25bd833911354571206cfb50b97d613a6cf2
thuylinh <thuy.linh22112016@gmail.com>
2024-06-19 15:58:27 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //안녕하세요
+        System.out.println("Hello world!");
+
+        //하로
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLine());
+                //
+    }
+}
